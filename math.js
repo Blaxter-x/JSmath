13bc664fcc1f465a47143071fb7e859d66881261
Andres Ulloa <andresguc@gmail.com>
2023-01-11 20:16:07 -0500

    adding circle claculation funciton

--- math.js
+++ math.js
@@ -38,10 +38,35 @@ console.log({
 
 function calcularTriangulo(lado1, lado2, base, altura) {
     return {
-        perimetro: lado1 + lado2  + base,
+        perimetro: lado1 + lado2 + base,
         area: (base * altura) / 2,
     }
 }
 
-console.groupEnd('Triangulo') 
+console.groupEnd('Triangulo');
+
+console.group('Circulo');
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    PI,
+    circunferencia,
+    areaCirculo,
+})
+
+function calcularCirculo(radio) {
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio, 2);
+    return {
+        circunferencia: diametro * Math.PI,
+        area: radioAlCuadrado * Math.PI,
+    };
+
+}
+console.groupEnd('Circulo');
 
